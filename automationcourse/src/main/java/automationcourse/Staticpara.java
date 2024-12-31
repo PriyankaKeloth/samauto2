fa22bbdb57a8b1318d537dfed34e05ac9a95f567
Ranil <Ranil@DESKTOP-V42TCMT>
2024-12-31 21:28:37 +0530

    pushing

---
+++ automationcourse/src/main/java/automationcourse/Staticpara.java
@@ -0,0 +1,25 @@
+package automationcourse;
+
+public class Staticpara {
+	public static void main (String args[]) {
+
+		Staticpara.add();
+		Staticpara.sub();
+	}
+	public static void add()
+	{
+	int a=5;
+	int b=2;
+	int d=a+b;
+	System.out.println(d);
+	}
+	public static void sub()
+	{
+	int b=2;
+	int c=7;
+	int e=c-b;
+	System.out.println(e);
+	}
+	
+}
+
